5aa04087d86fa76ffc909857cf054a3177c615ec
Marco Visin <marco@visin.ch>
2023-04-24 15:29:18 +0200

    Fixing missing tab permission

--- public/background.js
+++ public/background.js
@@ -3,20 +3,31 @@ chrome.tabs.onUpdated.addListener(
     function(tabId, changeInfo, tab) {
         // read changeInfo data and do something with it
         // like send the new url to contentscripts.js
-        if (changeInfo.url) {
-            console.log(changeInfo.url)
-            chrome.tabs.sendMessage( tabId, {
-                message: 'url_update',
-                url: changeInfo.url
-            })
-            return;
+        try {
+            if (changeInfo.url) {
+                if(!changeInfo.url.includes("www.linkedin.com")){
+                    return;
+                }
+                console.log(changeInfo.url)
+                chrome.tabs.sendMessage(tabId, {
+                    message: 'url_update',
+                    url: changeInfo.url
+                })
+                return;
+            }
+            if (tab.url) {
+                if(!tab.url.includes("www.linkedin.com")){
+                    return;
+                }
+                console.log(tab.url)
+                chrome.tabs.sendMessage(tabId, {
+                    message: 'url_update',
+                    url: tab.url
+                })
+            }
         }
-        if (tab.url) {
-            console.log(tab.url)
-            chrome.tabs.sendMessage( tabId, {
-                message: 'url_update',
-                url: tab.url
-            })
+        catch (e){
+            console.error(e);
         }
     }
 );
