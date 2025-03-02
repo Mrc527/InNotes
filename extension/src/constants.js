1939a65158829eb343407b9208b1ff53d869cce0
Marco Visin <marco@visin.ch>
2025-03-02 13:55:18 +0100

    Add mobile top card ID
    Adds a constant for the mobile top card ID. This allows the extension to identify the top card on mobile LinkedIn profiles.

--- extension/src/constants.js
+++ extension/src/constants.js
@@ -1,5 +1,6 @@
 export const localMode = process.env.REACT_APP_LOCAL === "false";
 export const TOP_CARD_CLASS_NAME = '#profile-content section.artdeco-card';//'pv-top-card';
+export const TOP_CARD_ID_MOBILE = 'profile-image-modal-wrapper';
 export const URL = "https://innotes.me/api"
 //export const URL = "http://localhost:3000/api"
 //export const URL = "http://localhost"
