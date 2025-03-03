df1db24e8a20c78e4049431ccf0db781b8b5af80
Marco Visin <marco@visin.ch>
2025-03-03 15:36:27 +0100

    Add Matomo analytics tracking code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +25,37 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Matomo */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var _paq = window._paq = window._paq || [];
+              /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+              _paq.push(['trackPageView']);
+              _paq.push(['enableLinkTracking']);
+              (function() {
+                var u="https://track.visin.ch/";
+                _paq.push(['setTrackerUrl', u+'m.php']);
+                _paq.push(['setSiteId', '13']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.async=true; g.src=u+'m.js'; s.parentNode.insertBefore(g,s);
+              })();
+            `,
+          }}
+        />
+        <noscript>
+          <p>
+            <img
+              referrerPolicy="no-referrer-when-downgrade"
+              src="https://track.visin.ch/m.php?idsite=13&amp;rec=1"
+              style={{ border: "0" }}
+              alt=""
+            />
+          </p>
+        </noscript>
+        {/* End Matomo Code */}
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
