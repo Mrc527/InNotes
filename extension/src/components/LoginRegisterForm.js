150bfc8af81fab1fa32eae63633a4f119d038d0b
Marco Visin <marco@visin.ch>
2025-03-04 22:08:43 +0100

    Use button for LinkedIn login to improve accessibility

--- extension/src/components/LoginRegisterForm.js
+++ extension/src/components/LoginRegisterForm.js
@@ -1,16 +1,24 @@
 import React from 'react';
 
-const LoginRegisterForm = ({handleLinkedInLogin}) => {
+const LoginRegisterForm = ({ handleLinkedInLogin }) => {
     return (
         <>
-            <div style={{border: '1px solid #ddd', borderRadius: '5px', padding: '10px', marginBottom: '10px'}}>
-                <div style={{fontWeight: 'bold', marginBottom: '5px'}}>Login</div>
+            <div style={{ border: '1px solid #ddd', borderRadius: '5px', padding: '10px', marginBottom: '10px' }}>
+                <div style={{ fontWeight: 'bold', marginBottom: '5px' }}>Login</div>
                 <center>
                     <div>
-                        <a href="#" onClick={handleLinkedInLogin}><img
-                          src="https://content.linkedin.com/content/dam/developer/global/en_US/site/img/signin-button.png"
-                          alt="Sign in with LinkedIn"
-                          style={{maxWidth: '180px'}}/></a>
+                        <button onClick={handleLinkedInLogin} style={{
+                            background: 'none',
+                            border: 'none',
+                            padding: 0,
+                            margin: 0,
+                            cursor: 'pointer'
+                        }}>
+                            <img
+                                src="https://content.linkedin.com/content/dam/developer/global/en_US/site/img/signin-button.png"
+                                alt="Sign in with LinkedIn"
+                                style={{ maxWidth: '180px' }} />
+                        </button>
                     </div>
                 </center>
             </div>
