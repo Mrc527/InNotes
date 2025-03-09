6116ec98969f42da30479e9ec8ac35142fdcd63b
Marco Visin <marco@visin.ch>
2025-03-09 08:06:39 +0100

    Download Chrome extension artifact in Safari workflow
    This commit modifies the Safari workflow to download the Chrome extension artifact built in the Chrome workflow. This allows the Safari workflow to use the Chrome extension code. It also adds an artifact upload step to the Chrome workflow.

--- extension/InNotes-Safari/ci_scripts/ci_pre_xcodebuild.sh
+++ extension/InNotes-Safari/ci_scripts/ci_pre_xcodebuild.sh
@@ -1,4 +1,32 @@
 #!/bin/zsh
-cd ..
-yarn install
-yarn build:extension
+# ci_pre_xcodebuild.sh
+
+# Define the artifact name
+artifact_name="chrome-extension"
+
+# Define the destination directory
+destination_dir="../build"
+
+# Create the destination directory if it doesn't exist
+mkdir -p "$destination_dir"
+
+# Download the artifact
+echo "Downloading Chrome Extension artifact..."
+curl -H "Authorization: Bearer ${GITHUB_TOKEN}" -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" -L "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/artifacts" > artifacts.json
+
+# Extract the artifact ID
+artifact_id=$(jq -r ".artifacts[] | select(.name == \"${artifact_name}\") | .id" artifacts.json)
+
+# Download the artifact zip file
+curl -H "Authorization: Bearer ${GITHUB_TOKEN}" -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" -L "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/artifacts/${artifact_id}/zip" -o chrome-extension.zip
+
+# Unzip the artifact
+echo "Unzipping Chrome Extension artifact..."
+unzip chrome-extension.zip -d "$destination_dir"
+
+# Remove the zip file
+rm chrome-extension.zip
+rm artifacts.json
+
+echo "Chrome Extension artifact downloaded and extracted to $destination_dir"
+
